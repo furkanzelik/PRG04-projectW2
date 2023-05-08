1ab84fdee37d84aa06a807270fb6c23911243214
Huseyin Furkan Ozcelik <furkanzelik@gmail.com>
2023-05-08 14:56:47 +0300

    push try

--- src/js/game.js
+++ src/js/game.js
@@ -2,6 +2,7 @@ import '../css/style.css'
 import { Actor, Engine, Vector } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
 
+
 export class Game extends Engine {
 
     constructor() {
@@ -16,18 +17,36 @@ export class Game extends Engine {
         fish.pos = new Vector(0, 300)
         fish.vel = new Vector(-10,0)
         this.add(fish)
+        fish.on("exitviewport", (event) => {
+            fish.pos = new Vector (600,100)
+        } )
 
         const fish1 = new Actor()
         fish1.graphics.use(Resources.Fish.toSprite())
         fish1.pos = new Vector(400, 300)
         fish1.vel = new Vector(0,0)
         this.add(fish1)
+        fish1.on("exitviewport", (event) => {
+            fish1.pos = new Vector (400,100)
+        } )
 
         const fish2 = new Actor()
         fish2.graphics.use(Resources.Fish.toSprite())
         fish2.pos = new Vector(400, 300)
         fish2.vel = new Vector(10,0)
         this.add(fish2)
+
+        fish2.enableCapturePointer = true
+        fish2.pointer.useGraphicsBounds = true
+        fish2.on("pointerup", (event) => { console.log(`je klikt op fish2`)})
+
+        fish2.on("pointerup", (event) =>{
+            fish2.pos = new Vector(200,200)
+        } )
+
+        fish2.on("exitviewport", (event) => {
+            fish2.pos = new Vector (500,100)
+        } )
     }
 }
 
